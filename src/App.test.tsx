3ceda649c4181a9a029127be99885f11af3e97f5
Rafael Bruno <rafaelbrunosiqueira@gmail.com>
2022-03-22 23:42:10 -0300

    Adicionar pacotes para resolver problemas com o babel

---
+++ src/App.test.tsx
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom';
+// import { render, screen } from '@testing-library/react';
+
+describe('[App]', () => {
+  test('Simple test', () => {
+    // Arrange
+    // Act
+    // Assert
+    expect(true).toBeTruthy();
+  });
+});
